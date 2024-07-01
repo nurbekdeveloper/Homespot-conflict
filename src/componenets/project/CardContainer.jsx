bea5ffcf537967fdaa9cc6fdea2bd140b5405b9f
nurbekdeveloper <nurbeknormatov_02@gmail.com>
2024-07-01 17:56:05 +0500

    add new

--- src/componenets/project/CardContainer.jsx
+++ src/componenets/project/CardContainer.jsx
@@ -1,6 +1,5 @@
 import React, { useEffect } from "react";
 import { Link, useNavigate } from "react-router-dom";
-import "./CardContainer.css";
 import { houses } from "../data/homes.db";
 import { useDispatch } from "react-redux";
 
@@ -12,43 +11,42 @@ const CardContainer = () => {
     window.scrollTo(0, 0);
   }, []);
   return (
-    <div className="card-container-wrapper mt-[100px]">
-      <h2 className="title mb-3 font-semibold text-2xl">
+    <div className="mt-24  mb-4">
+      <h2 className="mb-3 font-semibold text-2xl">
         Наши готовые Проекты
       </h2>
       <p>
         Посмотрите галерею наших завершенных проектов, чтобы вдохновиться и
         выбрать то, что подходит именно вам
       </p>
-      <div className="card-container">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-6 mt-6">
         {filterReady.map((card, index) => (
           <Link
             to={`/moreinformation/${card.url}`}
             key={index}
-            className="card-ready mt-[60px]"
-            // onClick={() => navigate(`moreinformation/${card.url}`)}
+            className="block bg-gray-100 p-6 rounded-lg shadow-lg hover:shadow-xl transition-shadow duration-300"
           >
-            <img src={card.image} alt={card.title} className="card-image" />
-            <div className="bg-gray-100 p-6 rounded-lg shadow-lg max-w-base font-sans">
+            <img src={card.image} alt={card.title} className="w-full h-48 object-cover rounded-t-lg" />
+            <div className="p-6">
               <h2 className="text-sm font-semibold mb-2">
                 Дом из {card.exname} К-2003
               </h2>
               <div className="text-3xl font-bold mb-4">6 676 778 ₽</div>
               <div className="grid grid-cols-2 gap-4">
-                <div className="">
+                <div className="flex items-center">
                   <i className="fas fa-home pr-2"></i>
                   <span>137 м²</span>
                 </div>
-                <div className="">
-                  <i className="fas fa-bed  pr-2"></i>
-                  <span>3 </span>
+                <div className="flex items-center">
+                  <i className="fas fa-bed pr-2"></i>
+                  <span>3</span>
                 </div>
-                <div className="">
-                  <i className="fas fa-layer-group  pr-2"></i>
+                <div className="flex items-center">
+                  <i className="fas fa-layer-group pr-2"></i>
                   <span>1 этаж</span>
                 </div>
-                <div className="">
-                  <i className="fas fa-ruler-combined  pr-2"></i>
+                <div className="flex items-center">
+                  <i className="fas fa-ruler-combined pr-2"></i>
                   <span>9x15</span>
                 </div>
               </div>
